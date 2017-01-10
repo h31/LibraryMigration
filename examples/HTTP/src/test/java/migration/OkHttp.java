f5d398de25fb32a46c8043da165900b6f58a1417
Artyom Aleksyuk <artyom.h31@gmail.com>
2017-01-10 15:50:38 +0300

    OkHttp POST example

--- examples/HTTP/src/test/java/migration/OkHttp.java
+++ examples/HTTP/src/test/java/migration/OkHttp.java
@@ -1,8 +1,7 @@
 package migration;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
+import okio.BufferedSink;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,6 +15,22 @@ public class OkHttp {
     @Test
     public void okhttp() throws IOException {
         OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://kspt.icc.spbstu.ru/media/css/new/forms.css")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String hash = DigestUtils.md5Hex(response.body().string());
+        Assert.assertEquals(hash, Main.MD5_HASH);
+    }
+
+    public void okhttpPost() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        String data = "Hi!";
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), data.getBytes());
 
         Request request = new Request.Builder()
                 .url("http://kspt.icc.spbstu.ru/media/css/new/forms.css")
