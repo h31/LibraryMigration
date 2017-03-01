07d36ed8e598637a6376ddd539b8fb5c0bf37912
Artyom Aleksyuk <artyom.h31@gmail.com>
2017-03-01 22:02:13 +0300

    More reliable make_traces.sh

--- examples/make_traces.sh
+++ examples/make_traces.sh
@@ -3,10 +3,22 @@
 src_dir=$(pwd)
 cd "$(dirname "$0")"
 
+echo "instagram-java-scraper..."
 cd instagram-java-scraper/
-./gradlew clean test
+count=0
+if ! ./gradlew clean test; then
+    echo "Failed 1 time";
+    if ! ./gradlew clean test; then
+        echo "Failed 2 times";
+        if ! ./gradlew clean test; then
+            echo "Failed 3 times";
+            return 1
+        fi
+    fi
+fi
 cd ../
 
+echo "HTTP..."
 cd HTTP/
 ./gradlew clean test
 cd ${src_dir}
