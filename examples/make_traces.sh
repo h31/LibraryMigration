d383932d10a3bf6a031e8a1f47549a305fcbc93c
Artyom Aleksyuk <artyom.h31@gmail.com>
2017-03-01 22:17:15 +0300

    make_traces.sh: enable debug output

--- examples/make_traces.sh
+++ examples/make_traces.sh
@@ -3,22 +3,10 @@
 src_dir=$(pwd)
 cd "$(dirname "$0")"
 
-echo "instagram-java-scraper..."
 cd instagram-java-scraper/
-count=0
-if ! ./gradlew clean test; then
-    echo "Failed 1 time";
-    if ! ./gradlew clean test; then
-        echo "Failed 2 times";
-        if ! ./gradlew clean test; then
-            echo "Failed 3 times";
-            return 1
-        fi
-    fi
-fi
+./gradlew --stacktrace --debug clean test
 cd ../
 
-echo "HTTP..."
 cd HTTP/
 ./gradlew clean test
 cd ${src_dir}
